21d04fa628451e751405a2e150377f2c6540d290
Adam E <adamjervin@gmail.com>
2019-06-28 19:20:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,7 @@
 class Dog 
-  def initialize
-    @name = name
-    @breed = breed
-    
-  end 
-  
+   
   def pets
     @name =  name 
     @breed = breed
     
-end 
-
-def pets
-  "#{name} #{breed}"
-end 
 end 
